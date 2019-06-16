1f0f4c598c3b3707cb124945d7efa2fa91118b87
talos <249946595@qq.com>
2019-06-16 11:09:41 +0000

    ~

--- HMM_Viterbi.py
+++ HMM_Viterbi.py
@@ -1,7 +1,23 @@
+import numpy as np
+
+
 def hmm_viterbi(obs, states, start_p, trans_p, emit_p):
-# obs观察序列
-# states隐状态
-# start_p初始概率
-# trans_p状态转移矩阵
-# emit_p发射概率
-# 假设输入序列数以及状态序列个数都为N
+    # obs观察序列
+    # states隐状态
+    # start_p初始概率
+    # trans_p状态转移矩阵
+    # emit_p发射概率（混淆矩阵）
+    # 假设观察序列数为X，序列种类为N，状态数为M,则obs长度为X，states长度为X，start_p长度为M，trans_p形状为MxM,emit_p形状为MxN
+    V = [{}]
+    for y in states:
+        V[0][y] = start_p[y] * emit_p[y][obs[0]]
+    for t in range[1, len(obs)]:
+        V.append({})
+        for y in states:
+            V[t][y] = max([(V[t - 1][y0] * trans_p[y0][y] * emit_p[y][obs[t]]) for y0 in states])
+            result = []
+    for vector in V:
+        temp = {}
+        temp[vector.keys()[np.argmax(vector.values())]] = max(vector.values())
+        result.append(temp)
+    return result
