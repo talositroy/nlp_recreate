68f70e52c9ded60b5b7bf36662ed3812fc71262c
talos <249946595@qq.com>
2019-06-22 15:31:07 +0000

    使用了正则表达式

--- udacity/2_2_9.py
+++ udacity/2_2_9.py
@@ -1,12 +1,22 @@
-import nltk
+from nltk.tokenize import word_tokenize
+from nltk.tokenize import sent_tokenize
+from nltk.corpus import stopwords
 
 file = open("input.txt", "r")
-text = file.read()
+text = file.read().lower()
 # 单词分割
-words = nltk.word_tokenize(text)
+words = word_tokenize(text)
+print('---------单词分割---------')
 print(words)
-print('---------')
 # 句子分割
-sents = nltk.sent_tokenize(text)
+sents = sent_tokenize(text)
+print('---------句子分割---------')
 print(sents)
-
+# 停用词
+stop_word = stopwords.words("english")
+print('---------停用词---------')
+print(stop_word)
+# 去除停用词
+words_s_ed = [w for w in words if w not in stop_word]
+print('---------去除停用词词---------')
+print(words_s_ed)
