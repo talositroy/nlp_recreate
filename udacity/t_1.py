301c2c1521f7ebcb0431b20ec5dc78be3ef91581
talos <249946595@qq.com>
2019-06-22 09:14:53 +0000

    使用了正则表达式

--- udacity/t_1.py
+++ udacity/t_1.py
@@ -1,19 +1,17 @@
 """Count words."""
 
-# text = 'As I was waiting, a man came out of a side room, and at a glance I was sure he must be Long John. His left leg was cut off close by the hip, and under the left shoulder he carried a crutch, which he managed with wonderful dexterity, hopping about upon it like a bird. He was very tall and strong, with a face as big as a ham—plain and pale, but intelligent and smiling. Indeed, he seemed in the most cheerful spirits, whistling as he moved about among the tables, with a merry word or a slap on the shoulder for the more favoured of his guests.'
 import re
+
 
 def count_words(text):
     """Count how many times each unique word occurs in text."""
     counts = dict()  # dictionary of { <word>: <count> } pairs to return
 
     # TODO: Convert to lowercase
-    text = text.replace(',', '')
-    text = text.replace('.', '')
     text = text.lower()
     # TODO: Split text into tokens (words), leaving out punctuation
     # (Hint: Use regex to split on non-alphanumeric characters)
-    text_list = text.split(' ')
+    text_list = re.split(r'\W+', text, 0)
     # TODO: Aggregate word counts using a dictionary
     counts[text_list[0]] = 1
     for i in range(1, len(text_list)):
@@ -26,7 +24,7 @@ def count_words(text):
 
 
 def test_run():
-    with open("udacity/input.txt", "r") as f:
+    with open("input.txt", "r") as f:
         text = f.read()
         counts = count_words(text)
         sorted_counts = sorted(counts.items(), key=lambda pair: pair[1], reverse=True)
