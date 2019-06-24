830ebe88bcbe096ed36aa7769b2401f02ec5b1b0
talos <249946595@qq.com>
2019-06-24 10:49:01 +0000

    正则表达式的练习

--- udacity/2_2_9-13.py
+++ udacity/2_2_9-13.py
@@ -2,29 +2,48 @@ from nltk.tokenize import word_tokenize
 from nltk.tokenize import sent_tokenize
 from nltk.corpus import stopwords
 from nltk import pos_tag, ne_chunk
+from nltk.stem.porter import PorterStemmer
+from nltk.stem.wordnet import WordNetLemmatizer
 
 file = open("input.txt", "r")
 text = file.read().lower()
+
 # 单词分割
 words = word_tokenize(text)
 print('---------单词分割---------')
 print(words)
+
 # 句子分割
 sents = sent_tokenize(text)
 print('---------句子分割---------')
 print(sents)
+
 # 停用词
 stop_word = stopwords.words("english")
 print('---------停用词---------')
 print(stop_word)
+
 # 去除停用词
 words_s_ed = [w for w in words if w not in stop_word]
 print('---------去除停用词词---------')
 print(words_s_ed)
+
 # 词性标注
 print('---------词性标注---------')
 print(pos_tag(words))
+
 # 命名实体识别
 print('---------命名实体识别---------')
 words2 = "Antonio joined Udacity Inc. in California."
 ne_chunk(pos_tag(word_tokenize(words2)))
+
+# 词干提取
+print('---------词干提取---------')
+stemmed = [PorterStemmer().stem(w) for w in words_s_ed]
+print(stemmed)
+
+# 词形还原
+print('---------词形还原---------')
+# lemmatize默认还原的词性为名词，pos可以指定要还原的词性
+lemmed = [WordNetLemmatizer().lemmatize(w, pos='v') for w in words]
+print(lemmed)
---
+++ 中文分词/re_test.py
@@ -0,0 +1,24 @@
+# 这是一个关于正则表达式的练习
+import re
+
+text = r"Formally, a regular expression is an algebraic notation for characterizing a set of \
+strings. Thustheycanbeusedtospecifysearchstringsaswellastodefinealanguagein \
+aformalway. We willbeginbytalkingaboutregularexpressionsasa wayofspecifying \
+searches in texts, and proceed to other uses. Section 2.3 shows that the use of just \
+three regular expression operators is sufficient to characterize strings, but we use the \
+more convenient and commonly-used regular expression syntax of the Perl language \
+throughout this section. Since common text-processing programs agree on most of the \
+syntax of regular expressions, most of what we say extends to all UNIX, Microsoft \
+Word, and WordPerfect regular expressions. Appendix A shows the few areas where \
+these programs differ from the Perl syntax."
+
+p_string = text.split('\\')
+
+# for i in range(len(p_string)):
+#     if (re.search('\s.\..\s', p_string[i])):
+#         print(i, ':::', p_string[i])
+
+
+for i in range(len(p_string)):
+    if (re.search('[fro].', p_string[i])):
+        print(i, ':::', p_string[i])
